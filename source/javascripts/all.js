52df56381ab64e2cc691b0c164db5b1106d0ddb9
Tom Wahlin <tom.wahlin@gmail.com>
2015-03-28 11:36:21 -0400

    Additional updates

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,6 +6,14 @@ $(document).ready(function() {
       $('.frame-instagram').toggleClass('frame-instagram-truth');
 	  return false;
     });
+    $('.reveal-truth-facebook').click(function() {
+      $('.frame-facebook').toggleClass('frame-facebook-truth');
+	  return false;
+    });
+    $('.reveal-truth-linkedin').click(function() {
+      $('.frame-linkedin').toggleClass('frame-linkedin-truth');
+	  return false;
+    });
 }); 
 
 
