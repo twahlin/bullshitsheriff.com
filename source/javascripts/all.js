5cda9ce33b0ac920f556979fbeadca7716821222
Tom Wahlin <tom.wahlin@gmail.com>
2015-03-29 15:19:01 -0400

    Smooth scroll

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -14,7 +14,21 @@ $(document).ready(function() {
       $('.frame-linkedin').toggleClass('frame-linkedin-truth');
 	  return false;
     });
+		
+	  //smooth scrolling
+		$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html,body').animate({
+		          scrollTop: target.offset().top
+		        }, 1000);
+		        return false;
+		      }
+		    }
+		  });
+		});
+		
 }); 
-
-
-//put in the login form: LOLZ THIS FORM DOESN'T EVEN WORK. APRIL FOOLS!
